e3b86477843abc33a51348f9c63e21b595145551
João Neto <joaolima882018@gmail.com>
2022-02-22 16:46:03 +0100

    Remoção da pasta login

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,11 +4,66 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @yield('styles')
+    <link rel="stylesheet" href="/asset/css/smarthome.css">
     <title>@yield('title')</title>
 </head>
-<body>
-    @yield('content')
+<body class="dark-theme">
+    <header>
+        <div class="container">
+            <div class="navbar">
+                <div class="navbar-group">
+                    <img src="#" alt="Smarthome logo" class="navbar-brand">
+                    <button class="navbar-toggle">Toogle</button>
+                </div>
+                <div class="navbar-group">
+                    Other functionality
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="main">
+        <div class="main-content">
+            <aside class="menu">
+                <div class="user">
+                    <img src="/asset/img/user.jpg" alt="User Image" class="user-img">
+                    <a href="#" class="user-name user-text">My Home</a>
+                    <p class="user-adress user-text">Luanda/Viana casa nº123</p>
+                </div>
+                <ul class="nav">
+                    <li class="nav-item active">
+                        <a href="#" class="nav-link">
+                            <span class="nav-icon"></span>
+                            <p class="nav-text">Overview</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <span class="nav-icon"></span>
+                            <p class="nav-text">Overview</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <span class="nav-icon"></span>
+                            <p class="nav-text">Overview</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <span class="nav-icon"></span>
+                            <p class="nav-text">Overview</p>
+                        </a>
+                    </li>
+                </ul>
+            </aside>
+            <main class="content">
+                @yield('content')
+            </main>
+        </div>
+        <div class="main-quick-acess">
+            quick acess
+        </div>
+    </div>
     @yield('js')
 </body>
 </html>
