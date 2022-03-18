f2d1d01bea80c8c1a13667f9807e0c67948ecc57
Joao-Neto-DH <87363071+Joao-Neto-DH@users.noreply.github.com>
2022-03-18 14:35:18 +0100

    Add files via upload

---
+++ resources/views/components/graphic-board.blade.php
@@ -0,0 +1,9 @@
+<div class="board-body">
+    <div class="board-header">
+        <h2 class="h6">{{ $title }}</h2>
+        <button class="btn">
+            <img src="/asset/img/more-icon.svg" alt="more icon">
+        </button>
+    </div>
+    {{ $slot }}
+</div>
---
+++ resources/views/components/status-bar.blade.php
@@ -0,0 +1,10 @@
+<li class="status-bar-item">
+    <span class="status-place">{{ $place }}</span>
+    <div class="status-info">
+        <p class="status-value">{{ $value }}</p>
+        <span class="status-icon">
+            <span class="status-icon-unity">{{ $unity }}</span>
+            <span class="status-icon-grow">{!! $grow !!}</span>
+        </span>
+    </div>
+</li>
